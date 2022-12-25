2b20b63f5a1544cd324d29348c4d9ef2d8a6feba
LDS196 <lihachevd87@gmail.com>
2022-12-25 21:20:17 +0400

    codewars

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 const numbers = [1, 2, 3, 4, 5];
 
-Array.prototype.square = function (){return this.map(el=> Math.pow(el,2))}
-Array.prototype.cube = function (){return this.map(el=> Math.pow(el,3))}
-Array.prototype.average = function (){return this.length?(this.reduce((sum,el)=>{return sum + el},0))/ this.length: NaN}
-Array.prototype.sum = function (){return this.reduce((sum,el)=>{return sum + el},0)}
-Array.prototype.even = function (){ return this.filter(el=> el % 2 === 0)}
-Array.prototype.odd = function (){return this.filter(el=> el % 2 !== 0)}
+Array.prototype.square = function () {
+    return this.map(el => Math.pow(el, 2))
+}
+Array.prototype.cube = function () {
+    return this.map(el => Math.pow(el, 3))
+}
+Array.prototype.average = function () {
+    return this.length ? (this.reduce((sum, el) => {
+        return sum + el
+    }, 0)) / this.length : NaN
+}
+Array.prototype.sum = function () {
+    return this.reduce((sum, el) => {
+        return sum + el
+    }, 0)
+}
+Array.prototype.even = function () {
+    return this.filter(el => el % 2 === 0)
+}
+Array.prototype.odd = function () {
+    return this.filter(el => el % 2 !== 0)
+}
 
-console.log(numbers.odd())
+const santa = {
+    sayHoHoHo: function () {
+        console.log('Ho Ho Ho!')
+    },
+    distributeGifts: function () {
+        console.log('Gifts for all!');
+    },
+    goDownTheChimney: function () {
+        console.log('*whoosh*');
+    }
+};
+const notSanta = {
+    sayHoHoHo: function () {
+        console.log('Oink Oink!')
+    }
+    // no distributeGifts() and no goDownTheChimney()
+};
+
+function isSantaClausable(obj) {
+return 'function' == typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney)
+}
+
+
