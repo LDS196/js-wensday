27a4ea57c5e55f321b5198da973bc8355548502d
LDS196 <lihachevd87@gmail.com>
2023-01-14 01:13:47 +0400

    codewars

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
-const numbers = [1, 2, 3, 4, 5];
-
-Array.prototype.square = function () {
-    return this.map(el => Math.pow(el, 2))
-}
-Array.prototype.cube = function () {
-    return this.map(el => Math.pow(el, 3))
-}
-Array.prototype.average = function () {
-    return this.length ? (this.reduce((sum, el) => {
-        return sum + el
-    }, 0)) / this.length : NaN
-}
-Array.prototype.sum = function () {
-    return this.reduce((sum, el) => {
-        return sum + el
-    }, 0)
-}
-Array.prototype.even = function () {
-    return this.filter(el => el % 2 === 0)
-}
-Array.prototype.odd = function () {
-    return this.filter(el => el % 2 !== 0)
-}
-
-const santa = {
-    sayHoHoHo: function () {
-        console.log('Ho Ho Ho!')
-    },
-    distributeGifts: function () {
-        console.log('Gifts for all!');
-    },
-    goDownTheChimney: function () {
-        console.log('*whoosh*');
+const obj1 = {
+    first: "1",
+    second: "2",
+    third: false,
+    fourth: ["anytime", 2, 3, 4, {first: "1", second: "2",}],
+    fifth: null,
+    sixth: undefined,
+    seventh: {
+        first: "1",
+        second: "2",
     }
-};
-const notSanta = {
-    sayHoHoHo: function () {
-        console.log('Oink Oink!')
-    }
-    // no distributeGifts() and no goDownTheChimney()
-};
-
-function isSantaClausable(obj) {
-return 'function' == typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney)
 }
 
+
+
+function strCount(obj) {
+    let count = 0
+    for (const key in obj) {
+        if (typeof obj[key] === "string") count++
+        if (typeof obj[key] == "object") count += strCount(obj[key])
+    }
+
+    return count
+}
+
+const arr = ["Jacob", "Alex"]
+
+function likes(names) {
+switch (names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default:  return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+}
+}
 
