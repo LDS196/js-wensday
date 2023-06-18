3371103732ee23730cd2b112b25c09dffc59a18f
LDS196 <lihachevd87@gmail.com>
2023-06-18 13:00:07 +0400

    task

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-function isStringRotated(source, test) {
-  if(source.length!== test.length){
-    return false
-  }
-  for (let i = 0; i < source.length; i++) {
-const rotate = source.slice(i,source.length) + source.slice(0,i)
-    if(rotate=== test){
-      return true
-    }
-  }
+function arraySubset(source, subset) {
+if(source.length < subset.length){
   return false
 }
+  for (let i = 0; i <subset.length ; i++) {
+   const index = source.indexOf(subset[i])
 
-console.log(isStringRotated('javascript', 'scriptjava')) // -> true
-console.log(isStringRotated('javascript', 'iptjavascr')) // -> true
-console.log(isStringRotated('javascript', 'java')) // -> false
+    if (index===-1){
+      return false
+    }
+    delete source[index]
+  }
+  return true
+}
+
+console.log(arraySubset([2, 1, 3], [1, 2, 3])) // -> true
+console.log(arraySubset([2, 1, 1, 3], [1, 2, 3])) // -> true
+console.log(arraySubset([1, 1, 1, 3], [1, 3, 3])) // -> false
+console.log(arraySubset([1, 2], [1, 2, 3])) // -> false
