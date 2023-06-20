cb9671933c683f4d9c14ab8f4f6cf812b257a420
LDS196 <lihachevd87@gmail.com>
2023-06-20 19:25:05 +0400

    task

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-
-const matrix = [
-    [ 6, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-function rotate(source) {
-    const res = source[0].map(()=>[])
-
-    for (let i = 0; i <matrix.length ; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-            res[matrix.length-1-i][matrix.length-1-j] = source[i][j]
+function isBalanced(string) {
+    const start = '{[('
+    const end = ')}]'
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    const queue = []
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i]
+        if(start.includes(char)){
+            queue.push(char)
+        } else if(end.includes(char)){
+            const last = queue.pop()
+            if(map[char]!== last){
+                return false
+            }
         }
     }
-    return res
+    return !queue.length
 }
+
+console.log(isBalanced('(x + y) - (4)')) // -> true
+console.log(isBalanced('(((10 ) ()) ((?)(:)))')) // -> true
+console.log(isBalanced('[{()}]')) // -> true
+console.log(isBalanced('(50)(')) // -> false
+console.log(isBalanced('[{]}')) // -> false
 
 
