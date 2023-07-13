d6d2cc3fbbed92809f31d7b2bb0e726dc822f0e6
LDS196 <lihachevd87@gmail.com>
2023-07-13 15:50:05 +0400

    task

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-let prices1 = [7,1,5,3,6,4]
-let prices2 = [7,5,5,2,1,1]
-
-const maxProfit = function (prices){
-    // let counter = 0
-    // for (let i = 0; i < prices.length ; i++) {
-    //     if(prices[i]< prices[i+1]){
-    //         counter= counter + (prices[i+1]-prices[i])
-    //     }
-    // }
-    // return counter
-    let profit = 0
-    prices.forEach((price,i,arr)=>{
-        if(price>arr[i-1]) profit=profit + price-arr[i-1]
-    })
-    return profit
+function isPalindrome(str){
+return str ===str.splice('').reverse().join('')
 }
-console.log(maxProfit(prices2))
+
