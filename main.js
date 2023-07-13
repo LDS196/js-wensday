f59be0b136787de1dff601d0282e33e852034bb0
LDS196 <lihachevd87@gmail.com>
2023-07-13 11:13:50 +0400

    task

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-let input1 = [[1, 3], [2, 6], [8, 10], [15, 18]]
-let input2 = [[1, 4], [4, 5],]
+let prices1 = [7,1,5,3,6,4]
+let prices2 = [7,5,5,2,1,1]
 
-function merge(intervals) {
-    if(intervals.length<2) return intervals
-    intervals.sort((a,b)=> a[0]-b[0])
-    let result = [intervals[0]]
-    for (const interval of intervals) {
-        let recent= result[result.length-1]
-        if(recent[1]>= interval[0]){
-            recent[1]=Math.max(recent[1],interval[1])
-
-        } else{
-            result.push(interval)
-        }
-    }
-    return result
+const maxProfit = function (prices){
+    // let counter = 0
+    // for (let i = 0; i < prices.length ; i++) {
+    //     if(prices[i]< prices[i+1]){
+    //         counter= counter + (prices[i+1]-prices[i])
+    //     }
+    // }
+    // return counter
+    let profit = 0
+    prices.forEach((price,i,arr)=>{
+        if(price>arr[i-1]) profit=profit + price-arr[i-1]
+    })
+    return profit
 }
-
-console.log(merge(input1))
+console.log(maxProfit(prices2))
