5a06e5cfea87ecb08988c53a4b1b17a587214035
LDS196 <lihachevd87@gmail.com>
2023-07-20 13:46:40 +0400

    task

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function toInitials(str){
-     const a = str.split(' ').map(el=> el.slice(0,1).toUpperCase() + '.').join('')
+let words = ['banana','grapefruit','banana','grapefruit','banana', 'orange', 'banana']
+
+function mySort(arr){
+    const obj={}
+    for (let i = 0; i < arr.length; i++) {
+        const key = arr[i]
+        if(obj.hasOwnProperty(key)){
+            obj[key]=obj[key] + 1
+        } else{
+            obj[key]=1
+        }
+    }
+    let res=[]
+    for (const objElement in obj) {
+        res.push({name:objElement,count:obj[objElement]})
+
+    }
+
+    return res.sort((a,b)=> b.count-a.count).map(el=> el.name)
 }
+
