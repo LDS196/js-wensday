be81208026855dd1a8c13cdbc948d79ce5b9c2a8
LDS196 <lihachevd87@gmail.com>
2023-08-10 13:30:50 +0400

    task

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const moveZeros = function (arr) {
-    return [
-        ...(arr.filter(n => n !== 0)),
-        ...(arr.filter(n => n === 0))
-    ];
+function pigIt(str){
+
+    return str
+        .split(' ')
+        .map(el=>{
+        if(isSpecialCharacter(el)) return el
+        else {
+            return el.slice(1,el.length) + el.slice(0,1) + 'ay'
+        }
+    })
+        .join(' ')
 }
-// function moveZeros(arr) {
-//     return arr.sort((a, b) => b === 0 ? -1 : 0);
-// }
-// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+function isSpecialCharacter(char) {
+    // Регулярное выражение для проверки, является ли символ специальным
+    const specialCharacterRegex = /[^\w\s]/;
+
+    return specialCharacterRegex.test(char);
+}
+
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
