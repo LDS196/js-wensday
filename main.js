8ad9d32a9b7dc44501e6b94952a815d07768b57a
LDS196 <lihachevd87@gmail.com>
2023-08-15 11:37:21 +0400

    task

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function componentToHex(c) {
-    let x= c
-    if(x>255)x=255
-    if(x<0)x=0
-    const hex = x.toString(16).toUpperCase();
-    return hex.length === 1 ? "0" + hex : hex;
+function dirReduc(arr) {
+     if(arr.length===1) return arr
+
+    let res = []
+
+    for (let i = 0; i < arr.length; i++) {
+        let current = arr[i]
+        let next = arr[i + 1]
+
+        if (current === "NORTH" && next === "SOUTH" || current === "SOUTH" && next === "NORTH") {
+            i = i + 1
+            continue
+        }
+        if (current === "WEST" && next === "EAST" || current === "EAST" && next === "WEST") {
+            i = i + 1
+            continue
+        }
+        res.push(current)
+    }
+    if(res.length===arr.length) return res
+    return  dirReduc(res)
 }
 
-function rgb(r, g, b) {
-    return componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-// 255, 255, 255 --> "FFFFFF"
-// 255, 255, 300 --> "FFFFFF"
-// 0, 0, 0       --> "000000"
-// 148, 0, 211   --> "9400D3"
+console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]))
