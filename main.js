c8e0960b8a8920cea1fcc52448a231753989ead7
LDS196 <lihachevd87@gmail.com>
2023-10-23 12:10:18 +0400

    task

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-function dirReduc(arr) {
-     if(arr.length===1) return arr
-
-    let res = []
-
-    for (let i = 0; i < arr.length; i++) {
-        let current = arr[i]
-        let next = arr[i + 1]
-
-        if (current === "NORTH" && next === "SOUTH" || current === "SOUTH" && next === "NORTH") {
-            i = i + 1
-            continue
+function sum(...args) {
+    function add(...nextArgs) {
+        if (nextArgs.length === 0) {
+            return args.reduce((acc, val) => acc + val, 0);
         }
-        if (current === "WEST" && next === "EAST" || current === "EAST" && next === "WEST") {
-            i = i + 1
-            continue
-        }
-        res.push(current)
+        return sum(...args, ...nextArgs);
     }
-    if(res.length===arr.length) return res
-    return  dirReduc(res)
+    return add;
 }
 
-console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]))
+console.log(sum(1)(2)( 4)()); // Выводит 10
+
+
