7788c98020cd22580c4b0d4506ff13780422fdeb
LDS196 <lihachevd87@gmail.com>
2023-10-24 22:51:49 +0400

    new task

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-const flatten = (items) => {
-    let result = []
-    for (let index = 0; index < items.length; index++) {
-        const item= item[index]
-        if(Array.isArray(item)){
-            result= result.concat(flatten(item))
-        } else{
-            result.push(item)
-        }
+class Categories {
+    constructor() {
+        this.categoryData = categoryData
     }
-    return result
-};
 
+    getChildren(categoryId) {
+        return this.categoryData
+            .filter(category => category.parent === categoryId)
+            .map(c => c.id)
+            .sort((a, b) => a - b)
+    }
+
+    getParent(categoryChildId) {
+        const category = this.categoryData.find(c => c.id === categoryChildId)
+        return category ? category.parent : undefined
+    }
+
+    getPath(categoryChildId) {
+
+        let path = []
+        for (let i = 0; i < this.categoryData.length; i++) {
+            if (this.categoryData[i].id <= categoryChildId) {
+                path.push(this.categoryData[i].id)
+            } else {
+                break
+            }
+        }
+        return path
+    }
+}
+
+const categoryData = [
+    {id: 1},
+    {id: 2, parent: 1},
+    {id: 3, parent: 2},
+    {id: 4, parent: 3},
+    {id: 5, parent: 3}
+];
+
+const categoryTree = new Categories(categoryData);
+
+console.log("Children of category 3:", categoryTree.getChildren(3)); // [4, 5]
+console.log("Parent of category 2:", categoryTree.getParent(2)); // 1
+console.log("Path to category 4:", categoryTree.getPath(4)); // [1, 2, 3, 4]
 
