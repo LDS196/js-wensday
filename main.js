5b46c24e8de4ecfb8d8936dba97daf9ea229fcca
LDS196 <lihachevd87@gmail.com>
2023-11-09 10:16:09 +0400

    new task

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-function isPrime(number) {
-    if (number <= 1) {
-        return false;
-    }
-
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
+const tank = [
+    {id: 1, type: 'mango', price: 35},
+    {id: 2, type: 'mango', price: 77},
+    {id: 3, type: 'potato', price: 84},
+    {id: 4, type: 'potato', price: 25},
+    {id: 5, type: 'mango', price: 33},
+    {id: 6, type: 'apple', price: 50},
+    {id: 7, type: 'mango', price: 89},
+    {id: 8, type: 'apple', price: 60},
+    {id: 9, type: 'mango', price: 51},
+    {id: 10, type: 'apple', price: 82},
+];
+// Задача 1: Написать функцию, создающую
+// новый массив из исходного. В новом массиве должны быть удалены
+// удалены все объекты с type: 'mango', а для 'apple' цены (значение поля price) умножены на 3.
+function getModifiedArray(arr, deletedType, ModifiedType, number) {
+    let res = []
+    arr.forEach(item=>{
+        if (item.type !== deletedType) {
+            if (item.type === ModifiedType) {
+                const copyItem ={...item}
+                copyItem.price = copyItem.price * number
+                res.push(copyItem)
+            } else {
+                res.push(item)
+            }
         }
-    }
-
-    return true;
+    })
+    return res
 }
 
-console.log(isPrime(2)); // true
-console.log(isPrime(12)); // false
-console.log(isPrime(23)); // true
-console.log(isPrime(100)); // false
+console.log(getModifiedArray(tank,'mango','apple',3),tank)
